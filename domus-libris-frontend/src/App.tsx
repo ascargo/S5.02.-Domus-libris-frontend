c493510a21550f842af50b6ee882820a69f37311
Asier Carrasco González <axiercarrasco@gmail.com>
2025-12-04 13:15:57 +0100

    set up

---
+++ domus-libris-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-slate-50 underline">
+        DOMUS LIBRIS FRONTEND
+      </h1>
+    </div>
+  );
+}
+
+export default App
